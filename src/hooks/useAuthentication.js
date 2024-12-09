656f3aa14a9b56d8776847c3f7ac47ad7fba4104
Ankit Gade <ankit.gade@10up.com>
2024-12-09 11:36:04 +0530

    Add redirect auth flow

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -1,19 +1,45 @@
-import { useCallback, useContext } from "react";
+import { useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-import { signInWithPopup } from "firebase/auth";
+import { signInWithRedirect, getRedirectResult } from "firebase/auth";
 import { googleProvider, auth } from "../services/index.js";
 import { useSession } from "../AuthContext.jsx";
 
 export function useAuthentication() {
   const navigate = useNavigate();
+  const { setUser } = useSession();
+
+  useEffect(() => {
+    const fetchRedirectResult = async () => {
+      try {
+        const result = await getRedirectResult( auth );
+        console.log("Redirect result:", result);
+        if (result) {
+          const { user } = result;
+          console.log("User authenticated:", user);
+
+          // Set the user in your context
+          setUser(user);
+
+          // Navigate to the homepage or another page
+          navigate("/");
+        }
+      } catch (error) {
+        console.error("Error retrieving redirect result:", error);
+      }
+    };
+
+    fetchRedirectResult();
+  }, [navigate, setUser]);
 
   const signIn = useCallback(async () => {
     try {
-      const result = await signInWithPopup( auth, googleProvider );
-      navigate("/");
-      return {};
+      await signInWithRedirect( auth, googleProvider );
+      return;
     } catch (error) {
-      console.error(error);
+      return {
+        error: 'Error logging in, please try again!',
+        type: 'googleSignIn',
+      };
     }
   }, [navigate]);
 
